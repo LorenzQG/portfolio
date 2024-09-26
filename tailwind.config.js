8fb3ff2000d9c519704fe119b991caa81783a0ae
Lorenzo <lorenzodequadrosgoncalves@gmail.com>
2024-09-26 19:26:14 -0300

    Tailwind and other packages

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      "colors": {
+        "primaryBG": "#0D0D0D",
+        "secondaryBG":  "#2C2C2C",
+        "primaryText": "#EAEAEA",
+        "secondaryText": "#8A8A8A",
+        "destaque": "#00BFAE"
+      },
+      "dropShadow":{
+        "img": "0 25px 25px rgba(0, 191, 174, 0.6)",
+        "text": "0 10px 10px rgba(0, 191, 174, 0.6)",
+      }
+    },
+  },
+  plugins: [],
+}
+
